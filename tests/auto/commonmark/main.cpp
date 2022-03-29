7a7462bfcf7c779dcb45e661b5288908690a899d
Igor Mironchik <igor.mironchik@gmail.com>
2022-03-29 13:19:13 +0300

    Add CommonMark test #138.

--- tests/auto/commonmark/main.cpp
+++ tests/auto/commonmark/main.cpp
@@ -2884,3 +2884,23 @@ TEST_CASE( "137" )
 		REQUIRE( c->text() == QStringLiteral( "aaa\n    ```" ) );
 	}
 }
+
+TEST_CASE( "138" )
+{
+	const auto doc = load_test( 138 );
+
+	REQUIRE( doc->isEmpty() == false );
+	REQUIRE( doc->items().size() == 2 );
+
+	REQUIRE( doc->items().at( 1 )->type() == MD::ItemType::Paragraph );
+	auto p = static_cast< MD::Paragraph* > ( doc->items().at( 1 ).data() );
+	REQUIRE( p->items().size() == 2 );
+	REQUIRE( p->items().at( 0 )->type() == MD::ItemType::Code );
+	auto c = static_cast< MD::Code* > ( p->items().at( 0 ).data() );
+	REQUIRE( c->inlined() );
+	REQUIRE( c->text() == QStringLiteral( " " ) );
+	REQUIRE( p->items().at( 1 )->type() == MD::ItemType::Text );
+	auto t = static_cast< MD::Text* > ( p->items().at( 1 ).data() );
+	REQUIRE( t->opts() == MD::TextWithoutFormat );
+	REQUIRE( t->text() == QStringLiteral( "aaa" ) );
+}
