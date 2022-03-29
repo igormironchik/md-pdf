130109dbb02afede7c2f1e04c02ad5bb00effa1d
Igor Mironchik <igor.mironchik@gmail.com>
2022-03-29 04:58:25 +0300

    Add CommonMark test #124.

--- tests/auto/commonmark/main.cpp
+++ tests/auto/commonmark/main.cpp
@@ -2667,3 +2667,19 @@ TEST_CASE( "123" )
 		REQUIRE( c->text() == QStringLiteral( "aaa\n```" ) );
 	}
 }
+
+TEST_CASE( "124" )
+{
+	const auto doc = load_test( 124 );
+
+	REQUIRE( doc->isEmpty() == false );
+	REQUIRE( doc->items().size() == 2 );
+
+
+	{
+		REQUIRE( doc->items().at( 1 )->type() == MD::ItemType::Code );
+		auto c = static_cast< MD::Code* > ( doc->items().at( 1 ).data() );
+		REQUIRE( !c->inlined() );
+		REQUIRE( c->text() == QStringLiteral( "aaa\n```" ) );
+	}
+}
