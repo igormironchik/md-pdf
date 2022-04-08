36db53f7e8209fed220c871dc5867695ae8e4c29
Igor Mironchik <igor.mironchik@gmail.com>
2022-04-08 13:50:52 +0300

    Add CommonMark test #208.

--- tests/auto/commonmark/main.cpp
+++ tests/auto/commonmark/main.cpp
@@ -3244,3 +3244,20 @@ TEST_CASE( "207" )
 
 	REQUIRE( doc->labeledLinks().size() == 1 );
 }
+
+TEST_CASE( "208" )
+{
+	const auto doc = load_test( 208 );
+
+	REQUIRE( doc->isEmpty() == false );
+	REQUIRE( doc->items().size() == 2 );
+
+	REQUIRE( doc->items().at( 1 )->type() == MD::ItemType::Paragraph );
+	auto p = static_cast< MD::Paragraph* > ( doc->items().at( 1 ).data() );
+	REQUIRE( p->items().size() == 1 );
+	REQUIRE( p->items().at( 0 )->type() == MD::ItemType::Text );
+	auto t = static_cast< MD::Text* > ( p->items().at( 0 ).data() );
+	REQUIRE( t->opts() == MD::TextWithoutFormat );
+	REQUIRE( t->text() == QStringLiteral( "bar" ) );
+	REQUIRE( doc->labeledLinks().size() == 1 );
+}
