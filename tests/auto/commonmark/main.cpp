979c9b19b9a3f42e96857617efddf6a21e1ef2d5
Igor Mironchik <igor.mironchik@gmail.com>
2022-04-08 13:30:58 +0300

    Add CommonMark test #205.

--- tests/auto/commonmark/main.cpp
+++ tests/auto/commonmark/main.cpp
@@ -3196,3 +3196,22 @@ TEST_CASE( "204" )
 	REQUIRE( doc->labeledLinks().contains( l->url() ) );
 	REQUIRE( doc->labeledLinks()[ l->url() ]->url() == QStringLiteral( "first" ) );
 }
+
+TEST_CASE( "205" )
+{
+	const auto doc = load_test( 205 );
+
+	REQUIRE( doc->isEmpty() == false );
+	REQUIRE( doc->items().size() == 2 );
+
+	REQUIRE( doc->items().at( 1 )->type() == MD::ItemType::Paragraph );
+	auto p = static_cast< MD::Paragraph* > ( doc->items().at( 1 ).data() );
+	REQUIRE( p->items().size() == 1 );
+	REQUIRE( p->items().at( 0 )->type() == MD::ItemType::Link );
+	auto l = static_cast< MD::Link* > ( p->items().at( 0 ).data() );
+	REQUIRE( l->img()->isEmpty() );
+	REQUIRE( l->textOptions() == MD::TextWithoutFormat );
+	REQUIRE( l->text() == QStringLiteral( "Foo" ) );
+	REQUIRE( doc->labeledLinks().contains( l->url() ) );
+	REQUIRE( doc->labeledLinks()[ l->url() ]->url() == QStringLiteral( "/url" ) );
+}
