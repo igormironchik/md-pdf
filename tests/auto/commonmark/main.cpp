619a72c2f4f286a688ee3cacb65c3e59f4332771
Igor Mironchik <igor.mironchik@gmail.com>
2022-04-09 13:44:43 +0300

    Add CommonMark test #220.

--- tests/auto/commonmark/main.cpp
+++ tests/auto/commonmark/main.cpp
@@ -3569,3 +3569,30 @@ TEST_CASE( "219" )
 		REQUIRE( t->text() == QStringLiteral( "bbb" ) );
 	}
 }
+
+TEST_CASE( "220" )
+{
+	const auto doc = load_test( 220 );
+
+	REQUIRE( doc->items().size() == 3 );
+
+	{
+		REQUIRE( doc->items().at( 1 )->type() == MD::ItemType::Paragraph );
+		auto p = static_cast< MD::Paragraph* > ( doc->items().at( 1 ).data() );
+		REQUIRE( p->items().size() == 1 );
+		REQUIRE( p->items().at( 0 )->type() == MD::ItemType::Text );
+		auto t = static_cast< MD::Text* > ( p->items().at( 0 ).data() );
+		REQUIRE( t->opts() == MD::TextWithoutFormat );
+		REQUIRE( t->text() == QStringLiteral( "aaa bbb" ) );
+	}
+
+	{
+		REQUIRE( doc->items().at( 2 )->type() == MD::ItemType::Paragraph );
+		auto p = static_cast< MD::Paragraph* > ( doc->items().at( 2 ).data() );
+		REQUIRE( p->items().size() == 1 );
+		REQUIRE( p->items().at( 0 )->type() == MD::ItemType::Text );
+		auto t = static_cast< MD::Text* > ( p->items().at( 0 ).data() );
+		REQUIRE( t->opts() == MD::TextWithoutFormat );
+		REQUIRE( t->text() == QStringLiteral( "ccc ddd" ) );
+	}
+}
