3d561c46d540910217ad377c6108cc78a046d925
Igor Mironchik <igor.mironchik@gmail.com>
2022-04-09 07:01:33 +0300

    Add CommonMark test #218.

--- tests/auto/commonmark/main.cpp
+++ tests/auto/commonmark/main.cpp
@@ -3498,3 +3498,21 @@ TEST_CASE( "217" )
 		REQUIRE( doc->labeledLinks()[ l->url() ]->url() == QStringLiteral( "/baz-url" ) );
 	}
 }
+
+TEST_CASE( "218" )
+{
+	const auto doc = load_test( 218 );
+
+	REQUIRE( doc->isEmpty() == false );
+	REQUIRE( doc->items().size() == 2 );
+
+	REQUIRE( doc->items().at( 1 )->type() == MD::ItemType::Paragraph );
+	auto p = static_cast< MD::Paragraph* > ( doc->items().at( 1 ).data() );
+	REQUIRE( p->items().size() == 1 );
+	REQUIRE( p->items().at( 0 )->type() == MD::ItemType::Link );
+	auto l = static_cast< MD::Link* > ( p->items().at( 0 ).data() );
+	REQUIRE( l->text() == QStringLiteral( "foo" ) );
+	REQUIRE( doc->labeledLinks()[ l->url() ]->url() == QStringLiteral( "/url" ) );
+
+	// I don't add empty blockquote to the document...
+}
