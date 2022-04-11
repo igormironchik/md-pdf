0f601b4220d1b189b28f73abcce48e91b90d984a
Igor Mironchik <igor.mironchik@gmail.com>
2022-04-11 16:38:11 +0300

    Add CommonMark test #243.

--- tests/auto/commonmark/main.cpp
+++ tests/auto/commonmark/main.cpp
@@ -4148,3 +4148,26 @@ TEST_CASE( "242" )
 		}
 	}
 }
+
+TEST_CASE( "243" )
+{
+	const auto doc = load_test( 243 );
+
+	REQUIRE( doc->items().size() == 2 );
+
+	{
+		REQUIRE( doc->items().at( 1 )->type() == MD::ItemType::Blockquote );
+		auto b = static_cast< MD::Blockquote* > ( doc->items().at( 1 ).data() );
+		REQUIRE( b->items().size() == 1 );
+
+		{
+			REQUIRE( b->items().at( 0 )->type() == MD::ItemType::Paragraph );
+			auto p = static_cast< MD::Paragraph* > ( b->items().at( 0 ).data() );
+			REQUIRE( p->items().size() == 1 );
+			REQUIRE( p->items().at( 0 )->type() == MD::ItemType::Text );
+			auto t = static_cast< MD::Text* > ( p->items().at( 0 ).data() );
+			REQUIRE( t->opts() == MD::TextWithoutFormat );
+			REQUIRE( t->text() == QStringLiteral( "foo bar" ) );
+		}
+	}
+}
