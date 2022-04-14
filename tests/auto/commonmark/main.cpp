92792abfd37128a7a5e0093422f74c94aa2ec4ee
Igor Mironchik <igor.mironchik@gmail.com>
2022-04-14 17:14:29 +0300

    Add CommonMark test #273.

--- tests/auto/commonmark/main.cpp
+++ tests/auto/commonmark/main.cpp
@@ -5124,3 +5124,41 @@ TEST_CASE( "272" )
 		REQUIRE( c->text() == QStringLiteral( "more code" ) );
 	}
 }
+
+TEST_CASE( "273" )
+{
+	const auto doc = load_test( 273 );
+
+	REQUIRE( doc->items().size() == 2 );
+
+	REQUIRE( doc->items().at( 1 )->type() == MD::ItemType::List );
+	auto l = static_cast< MD::List* > ( doc->items().at( 1 ).data() );
+	REQUIRE( l->items().size() == 1 );
+	REQUIRE( l->items().at( 0 )->type() == MD::ItemType::ListItem );
+	auto li = static_cast< MD::ListItem* > ( l->items().at( 0 ).data() );
+
+	REQUIRE( li->items().size() == 3 );
+	REQUIRE( li->listType() == MD::ListItem::Ordered );
+	REQUIRE( li->startNumber() == 1 );
+
+	{
+		REQUIRE( li->items().at( 0 )->type() == MD::ItemType::Code );
+		auto c = static_cast< MD::Code* > ( li->items().at( 0 ).data() );
+		REQUIRE( c->text() == QStringLiteral( "indented code" ) );
+	}
+
+	{
+		REQUIRE( li->items().at( 1 )->type() == MD::ItemType::Paragraph );
+		auto p = static_cast< MD::Paragraph* > ( li->items().at( 1 ).data() );
+		REQUIRE( p->items().size() == 1 );
+		REQUIRE( p->items().at( 0 )->type() == MD::ItemType::Text );
+		auto t = static_cast< MD::Text* > ( p->items().at( 0 ).data() );
+		REQUIRE( t->text() == QStringLiteral( "paragraph" ) );
+	}
+
+	{
+		REQUIRE( li->items().at( 2 )->type() == MD::ItemType::Code );
+		auto c = static_cast< MD::Code* > ( li->items().at( 2 ).data() );
+		REQUIRE( c->text() == QStringLiteral( "more code" ) );
+	}
+}
