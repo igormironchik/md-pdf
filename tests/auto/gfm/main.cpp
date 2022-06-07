0e10379d0f9a2b4e3ee480086b6125f3e9982ecd
Igor Mironchik <igor.mironchik@gmail.com>
2022-06-07 11:23:57 +0300

    Add GFM test #205.

--- tests/auto/gfm/main.cpp
+++ tests/auto/gfm/main.cpp
@@ -533,3 +533,43 @@ TEST_CASE( "204" )
 		}
 	}
 }
+
+TEST_CASE( "205" )
+{
+	const auto doc = load_test( 205 );
+	REQUIRE( doc->items().size() == 2 );
+
+	REQUIRE( doc->items().at( 1 )->type() == MD::ItemType::Table );
+	const auto t = static_cast< MD::Table* > ( doc->items().at( 1 ).data() );
+	REQUIRE( t->columnsCount() == 2 );
+	REQUIRE( t->rows().size() == 1 );
+
+	REQUIRE( t->columnAlignment( 0 ) == MD::Table::AlignLeft );
+	REQUIRE( t->columnAlignment( 1 ) == MD::Table::AlignLeft );
+
+	{
+		REQUIRE( t->rows().at( 0 )->type() == MD::ItemType::TableRow );
+		const auto r = static_cast< MD::TableRow* > ( t->rows().at( 0 ).data() );
+		REQUIRE( r->cells().size() == 2 );
+
+		{
+			REQUIRE( r->cells().at( 0 )->type() == MD::ItemType::TableCell );
+			const auto c = static_cast< MD::TableCell* > ( r->cells().at( 0 ).data() );
+			REQUIRE( c->items().size() == 1 );
+			REQUIRE( c->items().at( 0 )->type() == MD::ItemType::Text );
+			const auto t = static_cast< MD::Text* > ( c->items().at( 0 ).data() );
+			REQUIRE( t->opts() == MD::TextWithoutFormat );
+			REQUIRE( t->text() == QStringLiteral( "abc" ) );
+		}
+
+		{
+			REQUIRE( r->cells().at( 1 )->type() == MD::ItemType::TableCell );
+			const auto c = static_cast< MD::TableCell* > ( r->cells().at( 1 ).data() );
+			REQUIRE( c->items().size() == 1 );
+			REQUIRE( c->items().at( 0 )->type() == MD::ItemType::Text );
+			const auto t = static_cast< MD::Text* > ( c->items().at( 0 ).data() );
+			REQUIRE( t->opts() == MD::TextWithoutFormat );
+			REQUIRE( t->text() == QStringLiteral( "def" ) );
+		}
+	}
+}
