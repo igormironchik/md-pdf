4b1440aec546073b9f4cc9539d81030a58916bcd
Igor Mironchik <igor.mironchik@gmail.com>
2023-12-06 17:49:46 +0300

    Add md4qt version info into about dialog.

--- src/main_window.cpp
+++ src/main_window.cpp
@@ -393,9 +393,12 @@ MainWindow::about()
 	QMessageBox::about( this, tr( "About MD-PDF Converter" ),
 		tr( "MD-PDF Converter.\n\n"
 			"Version %1\n\n"
+			"md4qt version %2\n\n"
 			"Author - Igor Mironchik (igor.mironchik at gmail dot com).\n\n"
 			"Copyright (c) 2019-2022 Igor Mironchik.\n\n"
-			"Licensed under GNU GPL 3.0." ).arg( c_version ) );
+			"Licensed under GNU GPL 3.0." )
+				.arg( c_version )
+				.arg( c_md4qtVersion ) );
 }
 
 void
