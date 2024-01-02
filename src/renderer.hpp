4f0c440016662b9947fc63ecae77f1951f0bfc6f
Igor Mironchik <igor.mironchik@gmail.com>
2024-01-02 10:36:28 +0300

    Update license.

--- src/renderer.hpp
+++ src/renderer.hpp
@@ -4,7 +4,7 @@
 
 	\author Igor Mironchik (igor.mironchik at gmail dot com).
 
-	Copyright (c) 2019 Igor Mironchik
+	Copyright (c) 2019-2024 Igor Mironchik
 
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
