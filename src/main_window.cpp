3a52e48812232cff2add44b2749fef1c949d2017
Igor Mironchik <igor.mironchik@gmail.com>
2024-02-12 09:31:23 +0300

    Update copyright.

--- src/main_window.cpp
+++ src/main_window.cpp
@@ -395,7 +395,7 @@ MainWindow::about()
 			"Version %1\n\n"
 			"md4qt version %2\n\n"
 			"Author - Igor Mironchik (igor.mironchik at gmail dot com).\n\n"
-			"Copyright (c) 2019-2022 Igor Mironchik.\n\n"
+			"Copyright (c) 2019-2024 Igor Mironchik.\n\n"
 			"Licensed under GNU GPL 3.0." )
 				.arg( c_version )
 				.arg( c_md4qtVersion ) );
